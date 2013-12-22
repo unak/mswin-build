0a11c992892d3bbb25eacca4cd982fd373540c48
U.Nakamura <usa@garbagecollect.jp>
2013-12-23 03:57:51 +0900

    Fixed path problems.

--- bin/build.rb
+++ bin/build.rb
@@ -1,7 +1,6 @@
 #! ruby
 # expected ruby 1.9.x or later.
-
-$LOAD_PATH.unshift(File.join(File.dirname(File.dirname(__FILE__))), "lib")
+$LOAD_PATH.unshift(File.expand_path("../lib", File.dirname(__FILE__)))
 require "optparse"
 require "rbconfig"
 require "mswin-build/builder"
@@ -29,5 +28,5 @@ end
 # use this running ruby as BASERUBY
 baseruby = File.join(RbConfig::CONFIG["bindir"], RbConfig::CONFIG["ruby_install_name"] + RbConfig::CONFIG["EXEEXT"])
 
-builder = MswinBuild::Builder.new(target: target, baseruby: baseruby, settings: File.join(File.dirname(File.dirname(__FILE__)), "config/#{target}.yaml"))
+builder = MswinBuild::Builder.new(target: target, baseruby: baseruby, settings: File.expand_path("../config/#{target}.yaml", File.dirname(__FILE__)))
 exit builder.run
